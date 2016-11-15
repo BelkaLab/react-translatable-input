23b7f068434717ab22a193c0797ec66092bb8154
Giovanni Frigo <john.frigo92@gmail.com>
2016-11-15 18:20:43 +0100

    A more illustrative and clear example

--- examples/basic/index.jsx
+++ examples/basic/index.jsx
@@ -4,53 +4,131 @@ import TranslatableInput from 'react-translatable-input';
 
 import 'react-translatable-input/dist/react-translatable-input.css';
 
+import { getLanguageName } from 'language-cultures';
+
+// an helper function to translate language codes to language name.
+// in real life, this would be managed by an i18n manager such as polyglot.js
+function translateLanguage(lang) {
+  if (lang === 'default') {
+    return 'Default';
+  }
+  return getLanguageName(lang);
+}
+
+const demoLanguages = ['it-IT', 'en-US', 'de-DE'];
+
 class Demo extends React.Component {
   constructor(props) {
     super(props);
 
+    // Fill in demo data
+    const title = {
+      default: 'Default post title'
+    };
+    demoLanguages.forEach((c) => {
+      title[c] = `Post title in ${getLanguageName(c)}`;
+    });
+
+    const description = {
+      default: ''
+    };
+    demoLanguages.forEach((c) => {
+      description[c] = '';
+    });
+
+    const content = {
+      default: 'Default field is used when the user language is not supported/can\'t be detected'
+    };
+    demoLanguages.forEach((c) => {
+      content[c] = `Post content in ${getLanguageName(c)}`;
+    });
+
     this.state = {
-      title: {
-        'it-IT': 'Un componente input che permette di inserire il testo in varie lingue',
-        default: 'An input component that allows the input of multiple languages',
-        'de-DE': 'dasdad',
-        'en-US': 'An input component that allows the input of multiple languages'
-      },
-      editingLanguage: 'it-IT',
+      title,
+      description,
+      content,
+      editingLanguage: 'it-IT'
     };
   }
 
-  handleLocalizedChange(value, lang) {
-    const { title } = this.state;
-    title[lang] = value;
-    this.setState({
-      title
-    });
+  handleValueChange(value, lang, stateName) {
+    const state = this.state[stateName];
+    state[lang] = value;
+
+    const partialState = {};
+    partialState[stateName] = state;
+
+    this.setState(partialState);
   }
 
-  changeEditingLang(editingLanguage) {
+  handleLanguageChange(editingLanguage) {
     this.setState({
       editingLanguage
     });
   }
 
   render() {
-    const { title, editingLanguage } = this.state;
+    const { title, description, content, editingLanguage } = this.state;
     const nameError = false;
 
     return (
       <div>
-        <TranslatableInput
-          id="section-name-editor"
-          values={title}
-          lang={editingLanguage}
-          classes={nameError ? 'error' : ''}
-          onValueChange={(value, lang) => this.handleLocalizedChange(value, lang)}
-          onLanguageChange={lang => this.changeEditingLang(lang)}
-          onKeyDown={e => this.onKeyDown(e)}
-          showLanguageName={false}
-          disabled={false}
-          placeholder={'placeholder'}
-        />
+        <div className="line">
+          <h2>Create new post</h2>
+        </div>
+
+        <div className="line">
+          <label htmlFor="title">Title</label>
+          <label htmlFor="description">Description</label>
+          <TranslatableInput
+            id="title"
+            values={title}
+            lang={editingLanguage}
+            classes="inline"
+            onValueChange={(value, lang) => this.handleValueChange(value, lang, 'title')}
+            onLanguageChange={lang => this.handleLanguageChange(lang)}
+            showLanguageName
+            placeholder={'Post title'}
+          />
+          <TranslatableInput
+            id="description"
+            values={description}
+            lang={editingLanguage}
+            classes={!description[editingLanguage] ? 'error inline' : 'inline'}
+            onValueChange={(value, lang) => this.handleValueChange(value, lang, 'description')}
+            onLanguageChange={lang => this.handleLanguageChange(lang)}
+            langTranslator={lang => translateLanguage(lang)}
+            placeholder={'Post description'}
+          />
+        </div>
+
+        <div className="line">
+          <label htmlFor="content">Content</label>
+          <TranslatableInput
+            id="content"
+            values={content}
+            lang={editingLanguage}
+            classes={nameError ? 'error' : ''}
+            onValueChange={(value, lang) => this.handleValueChange(value, lang, 'content')}
+            onLanguageChange={lang => this.handleLanguageChange(lang)}
+            langTranslator={lang => translateLanguage(lang)}
+            showLanguageName
+            textarea
+            placeholder={'Post content'}
+          />
+        </div>
+
+        <div className="line">
+          <label htmlFor="metatag">Metatag</label>
+          <TranslatableInput
+            id="metatag"
+            values={{ default: 'Belka, React, Input, Translatable, Form, Multilanguage, Internazionalization, i18n' }}
+            lang="default"
+            langTranslator={lang => translateLanguage(lang)}
+            showLanguageName
+            disabled
+          />
+        </div>
       </div>
     );
   }
